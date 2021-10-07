1a9aac41057a7f43ede0ac0c80b03fdc1221a559
Jack Frederick <73072330+py1h0n-h4xer@users.noreply.github.com>
2021-10-07 12:40:56 -0500

    Update main.py
    Added base code

--- main.py
+++ main.py
@@ -1,1 +1,34 @@
+import time
 
+keepRuning = True
+currentFace = "None"
+global output
+output = "Error"
+
+#Face code
+
+def getFaceInfo():
+  global output
+  print("Get face name if avalible or set to none")
+  printDataOut()
+
+#Commands
+
+def getWeather():
+  global output
+  print("Get weather for weather comand")
+  printDataOut()
+
+def getTime():
+  global output
+  print("getTime")
+  printDataOut()
+  
+def printDataOut():
+  print(str(output))
+  
+  #Now for the fun stuff
+while keepRuning == True:
+  print("Main loop goes here")
+  getWeather()
+  
